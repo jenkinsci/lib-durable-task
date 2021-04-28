bd054d83169f821d1c28823ccde7266163d6dcbc
Carroll Chiou <cchiou@cloudbees.com>
2021-04-27 22:10:13 -0600

    turn of failFast

--- src/test-and-compile-binaries.bat
+++ src/test-and-compile-binaries.bat
@@ -1,8 +1,21 @@
 rem TODO
 setlocal
+
 rem maven plugin version
-set ver=%1
-set basedir=%cd%
-echo %ver%
-echo %basedir%
+set VER=%1
+set BASEDIR=%CD%
+set BIN=%BASEDIR%\bin
+rem destination of the test reports
+set TEST=%BASEDIR%\test-results
+rem failure file
+set FAILED=%TEST%\failed
+set NAME=durable_task_monitor
+
+echo %VER%
+echo %BASEDIR%
+echo %BIN%
+echo %TEST%
+echo %FAILED%
+echo %NAME%
+
 endlocal
