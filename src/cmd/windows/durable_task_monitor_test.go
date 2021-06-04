dea341e73d59d596aa4c22e714531dc28d41329a
Carroll Chiou <cchiou@cloudbees.com>
2021-06-04 00:02:51 -0600

    Disable heartbeat test code due to windows issues

--- src/cmd/windows/durable_task_monitor_test.go
+++ src/cmd/windows/durable_task_monitor_test.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
-	"os/signal"
-	"path/filepath"
 	"syscall"
-	"time"
-
-	"golang.org/x/sys/windows"
-	"jenkinsci.org/plugins/durabletask/common"
 )
 
 func sendSignal(pid int) {
@@ -31,10 +22,10 @@ func sendSignal(pid int) {
 
 // windows interrupt signals cannot be sent from a program.
 // see: github.com/golang/go/issues/6720
-// IMPORTANT: This test will fail in CI because:
-//   1) only works on windows container versions > ltsc2019 (https://github.com/docker/for-win/issues/3173)
-//   2) will still fail when being executed as RUN command in dockerfile
-// Test works correctly when running locally
+// IMPORTANT: This test is commented out because it will fail for the following reasons:
+//   1) will ALWAYS fail when executed as RUN command in dockerfile (even on > ltsc2019)
+//   2) code only works on windows versions > ltsc2019 (https://github.com/docker/for-win/issues/3173)
+/*
 func ExampleSignalCatcher() {
 	logger := log.New(os.Stdout, "", 0)
 	sigChan := make(chan os.Signal, 1)
@@ -111,3 +102,4 @@ func main() {
 	// Output:
 	// (sig catcher) caught: interrupt
 }
+*/
